f5483f92c2bd74fb49c6265b3e7265b841eed901
Rohan Shah <roshah2004@gmail.com>
2026-03-30 19:13:53 -0500

    tied backend w supabase api

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import os
 from dotenv import load_dotenv
+# Supabase client
+from supabase import create_client
 
 load_dotenv()
-
+sb_back = create_client(os.environ["SUPABASE_URL_BACK"], os.environ["SUPABASE_KEY_BACK"])
 app = Flask(__name__)
 CORS(app)
 
@@ -19,14 +21,14 @@ def search():
     if not query:
         return jsonify({"error": "Missing search query"}), 400
 
-    # placeholder response for now
-    return jsonify({
-        "query": query,
-        "results": [
-            {"title": "Inception"},
-            {"title": "Interstellar"}
-        ]
-    })
+    r = (
+        sb_back.table("movies")
+        .select("id, title, overview, genres, release_date")
+        .ilike("search_metadata", f"%{query}%")
+        .limit(20)
+        .execute()
+    )
+    return jsonify({"query": query, "results": r.data})
 
 if __name__ == "__main__":
     app.run(debug=True)
