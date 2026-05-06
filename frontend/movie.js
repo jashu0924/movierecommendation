a0fe078737be9796a306ac238cc9da5a605a6299
jashu0924 <nagajrduttala@gmail.com>
2026-05-05 19:43:20 -0500

    adding comments

--- frontend/movie.js
+++ frontend/movie.js
@@ -1,39 +1,49 @@
+//get the saved login/session information from localstorage
 const token = localStorage.getItem("access_token");
 const userEmail = localStorage.getItem("user_email");
 const userId = localStorage.getItem("user_id");
 
+//if anything missing make the user sign in again
 if (!token || !userEmail || !userId) {
   window.location.href = "login.html";
 }
 
+//connect the sign out button to the sign out function
 document.getElementById("user-signout-btn").addEventListener("click", signOut);
 
+//logs the user out by removing the session data
 function signOut() {
   localStorage.removeItem("access_token");
   localStorage.removeItem("user_email");
   localStorage.removeItem("user_id");
 
+  //redirecting user to the login page
   window.location.href = "login.html";
 }
 
+//read the movie id from the page url
 const params = new URLSearchParams(window.location.search);
 const movieId = params.get("id");
-
+//if no movie id in the url show an error
 if (!movieId) {
   document.getElementById("movie-details").innerHTML = "<p>No movie selected.</p>";
 } else {
   loadMovieDetails(movieId);
 }
 
+//getting movie details from the flask backend using the movie id
 async function loadMovieDetails(movieId) {
   const response = await fetch(`http://127.0.0.1:5000/movie/${movieId}`, {
     headers: {
+      //sends the user's access token so the backend can verify request
       "Authorization": `Bearer ${token}`
     }
   });
 
+  //convert the backend response into a JS object
   const movie = await response.json();
 
+  //error responses
   if (!response.ok) {
     document.getElementById("movie-details").innerHTML = `
       <p>Error loading movie: ${movie.error}</p>
@@ -41,8 +51,9 @@ async function loadMovieDetails(movieId) {
     return;
   }
 
+  //convert the list of genre objects into one readable string
   const genres = movie.genres.map(g => g.name).join(", ");
-
+  //building movie details for the movie pageusing the data returned from the backend
   document.getElementById("movie-details").innerHTML = `
     <div class="movie-details-layout">
       <img 
@@ -69,12 +80,12 @@ async function loadMovieDetails(movieId) {
       </div>
     </div>
   `;
-
+  //after the button is clicked, saves the movie to liked
   document.getElementById("like-movie-btn").addEventListener("click", () => {
     saveMovieLike(movie.id);
   });
 }
-
+//sends a POST request to the backend to save the users interaction
 async function saveMovieLike(movieId) {
   const status = document.getElementById("like-status");
   status.textContent = "Saving like...";
